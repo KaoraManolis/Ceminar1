c23a0ff2a8996e0641b31e4d67766a1e0b722e10
Kaora Manolis <kaoramanoliss@gmail.com>
2023-06-15 15:45:31 +0300

    три числа

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("input number =>");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("input number =>");
+int B = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("input number =>");
+int C = Convert.ToInt32(Console.ReadLine());
+
+
+if (A > B && A > C)
+{
+Console.WriteLine("A самое большое число");
+}
+
+else if (B > A && B > C)
+{
+Console.WriteLine("В самое большое число" );
+}
+else
+{
+Console.WriteLine("С самое большое число");
+}
